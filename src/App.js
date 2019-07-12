9bfa26b20521905e3b740b324605715a25db684d
John Squilinda <johnsquilinda@gmail.com>
2019-07-12 08:32:02 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Contact from "./Components/Contact";
+import Header from "./Components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="slinky" phone="5" email="haleyp@gmail.com" />
+          <Contact
+            name="winkly"
+            phone="1234567890"
+            email="winsquail@gmail.com"
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>The App</h1>
+//     </div>
+//   );
+// }
 
 export default App;
